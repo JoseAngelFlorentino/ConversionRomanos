595a1e6ba4bcf1a1abc00cd10d939bd620b0de21
JoseAngelFlorentino <144725919+JoseAngelFlorentino@users.noreply.github.com>
2024-05-16 10:34:33 -0400

    Unit Tests

--- ConversionRomanos.cpp
+++ ConversionRomanos.cpp
@@ -1,68 +1,69 @@
 #include <iostream>
-#include <stdio.h>
+#include <string>
 using namespace std;
 
-int main()
-{
-	int numero, unidades, decenas, centenas, millares;
+string decimalToRoman(int numero) {
+    int unidades, decenas, centenas, millares;
+    string resultado = "";
 
-	cout << "Introduce el numero decimal a convertir: ";
-	cin >> numero;
-	cout << "El numero en romano es: ";
+    unidades = numero % 10;
+    numero /= 10;
+    decenas = numero % 10;
+    numero /= 10;
+    centenas = numero % 10;
+    numero /= 10;
+    millares = numero % 10;
+    numero /= 10;
 
-	unidades = numero % 10;
-	numero /= 10;
-	decenas = numero % 10; 
-	numero /= 10;
-	centenas = numero % 10; 
-	numero /= 10;
-	millares = numero % 10;
-	numero /= 10;
+    switch (millares) {
+    case 1: resultado += "M"; break;
+    case 2: resultado += "MM"; break;
+    case 3: resultado += "MMM"; break;
+    }
 
-	switch (millares) {
-	case 1: cout << "M"; break;
-	case 2: cout << "MM"; break;
-	case 3: cout << "MMM"; break;
+    switch (centenas) {
+    case 1: resultado += "C"; break;
+    case 2: resultado += "CC"; break;
+    case 3: resultado += "CCC"; break;
+    case 4: resultado += "CD"; break;
+    case 5: resultado += "D"; break;
+    case 6: resultado += "DC"; break;
+    case 7: resultado += "DCC"; break;
+    case 8: resultado += "DCCC"; break;
+    case 9: resultado += "CM"; break;
+    }
 
-	}
+    switch (decenas) {
+    case 1: resultado += "X"; break;
+    case 2: resultado += "XX"; break;
+    case 3: resultado += "XXX"; break;
+    case 4: resultado += "XL"; break;
+    case 5: resultado += "L"; break;
+    case 6: resultado += "LX"; break;
+    case 7: resultado += "LXX"; break;
+    case 8: resultado += "LXXX"; break;
+    case 9: resultado += "XC"; break;
+    }
 
-	switch (centenas) {
-	case 1: cout << "C"; break;
-	case 2: cout << "CC"; break;
-	case 3: cout << "CCC"; break;
-	case 4: cout << "CD"; break;
-	case 5: cout << "D"; break;
-	case 6: cout << "DC"; break;
-	case 7: cout << "DCC"; break;
-	case 8: cout << "DCCC"; break;
-	case 9: cout << "CM"; break;
-	}
+    switch (unidades) {
+    case 1: resultado += "I"; break;
+    case 2: resultado += "II"; break;
+    case 3: resultado += "III"; break;
+    case 4: resultado += "IV"; break;
+    case 5: resultado += "V"; break;
+    case 6: resultado += "VI"; break;
+    case 7: resultado += "VII"; break;
+    case 8: resultado += "VIII"; break;
+    case 9: resultado += "IX"; break;
+    }
 
-	switch (decenas) {
-	case 1: cout << "X"; break;
-	case 2: cout << "XX"; break;
-	case 3: cout << "XXX"; break;
-	case 4: cout << "XL"; break;
-	case 5: cout << "L"; break;
-	case 6: cout << "LX"; break;
-	case 7: cout << "LXX"; break;
-	case 8: cout << "LXXX"; break;
-	case 9: cout << "XC"; break;
-	}
-
-	switch (unidades) {
-	case 1: cout << "I"; break;
-	case 2: cout << "II"; break;
-	case 3: cout << "III"; break;
-	case 4: cout << "IV"; break;
-	case 5: cout << "V"; break;
-	case 6: cout << "VI"; break;
-	case 7: cout << "VII"; break;
-	case 8: cout << "VIII"; break;
-	case 9: cout << "IX"; break;
-	}
-
-	cout << endl;
-	getchar();
+    return resultado;
 }
 
+int main() {
+    int numero;
+    cout << "Introduce el numero decimal a convertir: ";
+    cin >> numero;
+    cout << "El numero en romano es: " << decimalToRoman(numero) << endl;
+    return 0;
+}
---
+++ ConversionRomanosCabecera.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+#include <string>
+
+std::string decimalToRoman(int numero);
+
+#endif /
